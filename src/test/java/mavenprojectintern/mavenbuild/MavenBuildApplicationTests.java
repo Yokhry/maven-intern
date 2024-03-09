199e48b78f1d7d49f6434dcf33db1fbe1cbf6c99
Pratham Baliyan <104058723+Yokhry@users.noreply.github.com>
2024-03-10 01:47:06 +0530

    Add files via upload

---
+++ src/test/java/mavenprojectintern/mavenbuild/MavenBuildApplicationTests.java
@@ -0,0 +1,13 @@
+package mavenprojectintern.mavenbuild;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenBuildApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
